f6999cf58efbd2564d736744865c61792cf3ccd1
rayyyrayy <raymondaw02@gmail.com>
2025-11-09 05:46:15 -0500

    Created Script for door. Have socket for picture.
    Next step is to add key and key logic for specifc door. Then do rest of the doors in the house

---
+++ Assets/Scripts/DoorAnim.cs
@@ -0,0 +1,49 @@
+using NUnit.Framework;
+using UnityEngine;
+
+public class DoorAnim : MonoBehaviour
+{
+    public bool isClosed=true;
+    public bool isLocked;
+    private Animator doorAnim;
+    public GameObject key;
+    public GameObject _lock;
+
+    void Awake()
+    {
+        doorAnim = GetComponent<Animator>();
+        if (key == null || _lock == null)
+        {
+            Debug.Log("No Key or Lock");
+        }
+    }
+    
+    public void ToggleDoor()
+    {
+        
+        {
+            if (isClosed && !isLocked)
+            {
+                doorAnim.Play("Door_Open");
+                isClosed = false;
+            }
+            else if (!isClosed)
+            {
+                doorAnim.Play("Door_Close");
+                isClosed = true;
+            }
+            else
+            {
+                Debug.Log("Door is locked!");
+            }
+
+        }
+        }
+    
+    public void UnlockDoor()
+    {
+        isLocked = false;
+        Destroy(key);
+        Destroy(_lock);
+    }
+}
